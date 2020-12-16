510fa65ea78e03d8c1c38a971fcfb699291fdfa5
Your Name <you@example.com>
2020-12-16 09:45:30 +0700

    adđ file webmypham

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+	$('#slide').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+	autoplay:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:true
+        }
+    }
+});
+$('#btnmenu').click(function(){
+    $('#menutop').slideToggle('fast');
+});
+});
+$(document).ready(function(){
+	$('#th-pr').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    navText:["<img src='images/nav.png'>","<img src='images/nav-next.png'>"],
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:true
+        }
+    }
+});
+})
